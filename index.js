a63a38496a6f5027f9c4b20651af063c454fcb24
xedom <xedom08@gmail.com>
2020-03-21 14:49:40 +0100

    code rework

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ let fraseScelta = 0;
 let intervalloDomande;
 
 const attesa = (delayAttesa) => new Promise((resolve) => { setTimeout(resolve, delayAttesa*1000)});
-const splitFrase = s => s.includes('|') ? s.split('|') : [s];
+const splitPhrase = s => s.includes('|') ? s.split('|') : [s];
+const printPhrases = async (phrase) => {
+    const phraseArray = splitPhrase(phrase);
+    for (let i = 0; i < phraseArray.length; i++) {
+        client.say(`${cfg.channels[0]}`, phraseArray[i]);
+        await attesa(2);
+    }
+};
 
 const client = new tmi.Client({
     options: { debug: true },
@@ -23,22 +30,14 @@ client.on('connected', (add, port) => {
         if (!ready) return;
         ready = false;
 
-        let frasi = splitFrase(msg.phrases[fraseScelta].question);
-        for (let i = 0; i < frasi.length; i++) {
-            client.say(`${cfg.channels[0]}`, frasi[i]);
-            await attesa(2);
-        };
+        await printPhrases(msg.phrases[fraseScelta].question);
 
         await attesa(msg.phrases[fraseScelta].delay);
 
-        let riposte = splitFrase(msg.phrases[fraseScelta].answer);
-        for (let i = 0; i < riposte.length; i++) {
-            client.say(`${cfg.channels[0]}`, riposte[i]);
-            await attesa(2);
-        };
+        await printPhrases(msg.phrases[fraseScelta].answer);
 
         ready = true;
-        fraseScelta = fraseScelta <= msg.phrases.length-1 ? fraseScelta++ : 0;
+        fraseScelta = fraseScelta < msg.phrases.length-1 ? fraseScelta+1 : 0;
 
     }, msg.cooldown*1000)
 });
